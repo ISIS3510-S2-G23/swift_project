f45063e803962d8e4b7467e66be0acd3e4cae7ce
juanspardor <j.pardor@uniandes.edu.co>
2025-03-10 22:18:32 -0500

    Complete Points View structure (no tabbar, no within views

--- swift_app/swift_app/Points/Views/PointsView.swift
+++ swift_app/swift_app/Points/Views/PointsView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+
 struct PointsView: View {
     @State private var searchText: String = ""
     @State private var selectedTab: String = "Near Me"
@@ -14,30 +15,39 @@ struct PointsView: View {
     
     var body: some View {
         VStack{
-            HStack{
+            HStack {
                 Text("Recycle points in Bogotá")
                     .font(.title2)
+                    .foregroundStyle(.ecoMainPurple)
                     .fontWeight(.bold)
-                    .padding(.top, 15)
-                    .padding(.leading, 15)
+                
                 Spacer()
+                
+                Image(.person8)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 65, height: 65)
+                    
             }
+            .padding(.horizontal, 20)
             TextField("Find locations", text: $searchText)
                 .padding(15)
-                .background(Color(.systemGray6))
+                .background(Color(.ecoLightPurple))
                 .cornerRadius(29)
                 .padding(.horizontal, 15)
                 .overlay(
                     HStack {
                         Spacer()
                         Image(systemName: "magnifyingglass")
-                            .foregroundColor(.gray)
+                            .foregroundColor(.ecoMainPurple)
                             .padding(.trailing, 40)
                     }
                 )
+                .foregroundStyle(.ecoMainPurple)
             HStack{
                 Text("Choose your preference")
                     .font(.headline)
+                    .foregroundStyle(.ecoChooseBlue)
                     .padding(.top, 10)
                     .padding(.bottom, 10)
                     .padding(.leading, 20)
@@ -52,13 +62,13 @@ struct PointsView: View {
                     }) {
                         Text(category)
                             .fontWeight(selectedTab == category ? .bold : .regular)
-                            .foregroundColor(.purple)
+                            .foregroundColor(.ecoMainPurple)
                             .padding(.vertical, 15)
                             .frame(maxWidth: .infinity)
                             .overlay(
                                 Rectangle()
                                     .frame(height: 3)
-                                    .foregroundColor(selectedTab == category ? .purple.opacity(1) : .purple.opacity(0.3)),
+                                    .foregroundColor(selectedTab == category ? .ecoMainPurple.opacity(1) : .ecoMainPurple.opacity(0.3)),
                                 alignment: .bottom
                             )
                     }
@@ -67,6 +77,7 @@ struct PointsView: View {
             }
             .padding(.horizontal, 10)
             Spacer()
+            
         }
         
     }
