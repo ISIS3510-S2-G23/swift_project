757a9076784156847d52788c4be991ae68fc6280
juanspardor <j.pardor@uniandes.edu.co>
2025-03-15 22:41:08 -0500

    Added mock locations

--- swift_app/swift_app/Points/Near Me/MapVIewController.swift
+++ swift_app/swift_app/Points/Near Me/MapVIewController.swift
@@ -25,13 +25,28 @@ class MapViewController: UIViewController {
             mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
         
-        // Set an initial location (Example: New York City)
+        // Set an initial location (DO NOT CHANGE)
         let initialLocation = CLLocationCoordinate2D(latitude: 4.602978147636273, longitude: -74.06520670796498)
         let region = MKCoordinateRegion(
             center: initialLocation,
             span: MKCoordinateSpan(latitudeDelta: 0.0015, longitudeDelta: 0.0015)
         )
         mapView.setRegion(region, animated: true)
+        
+        // Add predefined locations (Recycle Points)
+        let locations: [(name: String, latitude: Double, longitude: Double)] = [
+            ("Recycling Center 1", 4.602800347961165, -74.0659410545649),
+            ("Recycling Center 2", 4.60349359373104, -74.06544967642769),
+            ("Eco Point", 4.603940686676833, -74.06591333578002)
+        ]
+
+        for location in locations {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
+            annotation.title = location.name
+            annotation.subtitle = "Recycle Point"
+            mapView.addAnnotation(annotation)
+        }
     }
 }
 
